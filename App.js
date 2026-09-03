c4b720a713a7f48d462dfa722680c5563426bd68
Wellington Menezes <Wellington Menezes>
2026-09-03 08:41:24 -0300

    app de tarefas

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitulo}>Tarefas de Estudo</Text>
+        <Text>2 Tarefas Pendentes</Text>
+      </View>
+    </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FAF6EE'
   },
+  headerTitulo: {
+    fontSize: 26,
+    fontWeight: 'bold'
+  },
+  header: {
+    paddingTop: 16,
+    paddingBottom: 12,
+    paddingHorizontal: 24
+  }
 });
